fbc73669eafbab3f7280710188a02ceb0cdce2fe
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2011-09-22 13:41:11 +0200

    add 2 operator tests

--- lib/yaclic.rb
+++ lib/yaclic.rb
@@ -1,7 +1,7 @@
+require 'yaclic/my_debug'
 require 'yaclic/colored'
 require 'yaclic/environment'
 require 'yaclic/expression_pointer'
 require 'yaclic/expression'
 require 'yaclic/operators'
 require 'yaclic/prompt'
-require 'yaclic/my_debug'
--- test/test_operation.rb
+++ test/test_operation.rb
@@ -6,19 +6,31 @@ class OperationTest < Test::Unit::TestCase
 	def test_add
 		o = OperatorAdd.new(10,5)
 
-		assert_equal o.apply_operator, 15
+		assert_equal 15, o.apply_operator
 	end
 
 	def test_mul
 		o = OperatorMul.new(10,5)
 		
-		assert_equal o.apply_operator, 50
+		assert_equal 50, o.apply_operator
 	end
 
 	def test_pow
 		o = OperatorPow.new(10,2)
 
-		assert_equal o.apply_operator, 100
+		assert_equal 100, o.apply_operator
+	end
+
+	def test_add_inv
+		o = OperatorAdd.new(10,ExpressionPointer.new(OperatorAddInv.new(5)))
+
+		assert_equal 5, o.apply_operator
+	end
+
+	def test_mul_inv
+		o = OperatorMul.new(10,ExpressionPointer.new(OperatorMulInv.new(5)))
+
+		assert_equal 2, o.apply_operator
 	end
 	
 end
