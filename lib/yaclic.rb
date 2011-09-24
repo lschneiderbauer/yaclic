5691c6d1fb870cf5d0b4be7750de67fc209f911f
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2011-09-24 14:45:26 +0200

    moved constants.rb, operators.rb to yaclic.rb

--- lib/yaclic.rb
+++ lib/yaclic.rb
@@ -2,7 +2,24 @@ require 'yaclic/my_debug'
 require 'yaclic/colored'
 require 'yaclic/expression_pointer'
 require 'yaclic/expression'
-require 'yaclic/operators'
-require 'yaclic/constants'
+
+# Operators
+require 'yaclic/operators/cannot_calculate_error.rb'
+require 'yaclic/operators/operator_num.rb'
+require 'yaclic/operators/operator_nil.rb'
+require 'yaclic/operators/binary_operator'
+require 'yaclic/operators/operator_add.rb'
+require 'yaclic/operators/operator_mul.rb'
+require 'yaclic/operators/operator_pow.rb'
+require 'yaclic/operators/operator_sin.rb'
+require 'yaclic/operators/operator_cos.rb'
+require 'yaclic/operators/operator_add_inv.rb'
+require 'yaclic/operators/operator_mul_inv.rb'
+
+# Constants
+require 'yaclic/constants/const_pi'
+require 'yaclic/constants/const_e'
+
+
 require 'yaclic/environment'
 require 'yaclic/prompt'
--- lib/yaclic/constants.rb
+++
@@ -1,2 +0,0 @@
-require 'yaclic/constants/const_pi'
-require 'yaclic/constants/const_e'
