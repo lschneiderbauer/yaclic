a15956caaf6531acbc314b25af0b1a2c9b235f56
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2011-10-20 19:03:59 +0200

    add kernel tests

---
+++ test/test_kernel.rb
@@ -0,0 +1,32 @@
+require 'test/unit'
+require 'yaclic'
+
+class KernelTest < Test::Unit::TestCase
+
+	def test_clone
+
+		kernel = Yaclic::Kernel.new
+
+		kernel.evaluate("a << e+f")
+		kernel.evaluate("e << 3")
+		kernel.evaluate("f << 8")
+
+		new_kernel = kernel.clone
+
+		assert_equal "11", new_kernel.evaluate("a.c")
+
+	end
+
+	def test_set
+
+		kernel = Yaclic::Kernel.new
+
+		kernel.evaluate("a << e+f")
+		kernel.set(:e, 3)
+		kernel.set(:f, Rational(1/3))
+
+		assert_equal "10/3", kernel.evaluate("a.c")
+
+	end
+
+end
