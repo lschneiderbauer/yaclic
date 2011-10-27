dfdb62e8ee005e18abb6330b86f3168a619b0f47
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2011-10-27 18:27:57 +0200

    it seems i was drunk ?! - fix

--- lib/yaclic/index.rb
+++ lib/yaclic/index.rb
@@ -1,8 +1,12 @@
 module Yaclic
 class Index < Hash
 
+	def self.[](sym)
+		super
+	end
+
 	def to_s
-		self.keys.map{|sym| "#{sym}".rjust(10).bold.cyan + " | ".blue + "#{@set[key].operation}" }.join "\n"
+		keys.map{|sym| "#{sym}".rjust(10).bold.cyan + " | ".blue + "#{self.[](sym).operation}" }.join "\n"
 	end
 
 end
